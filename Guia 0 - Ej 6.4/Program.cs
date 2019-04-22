9411e9ef83a1fb7ef374ac5d9eda3d3816646ea7
tomas.a.cerezo@outlook.com <tomas.a.cerezo@outlook.com>
2019-04-22 17:06:58 -0300

    Agregar archivos de proyecto.

---
+++ Guia 0 - Ej 6.4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guia_0___Ej_6._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
